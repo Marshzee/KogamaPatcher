bd47210d7b60f97f60b055db98a4573223c27b29
Evernight <209344383+Marshzee@users.noreply.github.com>
2026-08-27 20:22:32 +0000

    Update placeholder.asm

--- placeholder.asm
+++ placeholder.asm
@@ -1,1 +1,21 @@
+section .text
+global _start
 
+_start:
+    mov     rax, 42
+    xor     rbx, rbx
+    add     rbx, rax
+    imul    rbx, 7
+    sub     rbx, 294
+
+    cmp     rbx, 0
+    jne     .something_went_wrong
+
+    mov     rdi, 0
+    mov     rax, 60
+    syscall
+
+.something_went_wrong:
+    mov     rdi, 1
+    mov     rax, 60
+    syscall
